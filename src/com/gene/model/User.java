a813e86210228c1b232e067dda45928b98c36e25
GTabala <gtabala@gmail.com>
2022-08-19 13:20:39 +0100

    Added constructors

--- src/com/gene/model/User.java
+++ src/com/gene/model/User.java
@@ -3,6 +3,18 @@ package com.gene.model;
 public class User {
 	private String name;
 	private int age;
+	
+	
+
+	public User() {
+		super();
+	}
+
+	public User(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
 
 	public String getName() {
 		return name;
